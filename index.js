93ce1445ad34c0f739dbfbed4e00da1f12b21d73
Isal <58117968+rfzsal@users.noreply.github.com>
2022-09-05 14:05:52 +0700

    Upload

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-require('./routers')(app);
-require('./listeners')();
+// require('./routers')(app);
+// require('./listeners')();
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening to port ${port}...`));
